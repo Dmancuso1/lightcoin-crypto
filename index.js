511503b2d5e4eafcb9d5a27fdcf02dfd3134acd5
Dane Mancuso <danegmancuso@gmail.com>
2020-07-04 00:16:06 +0000

    impleted account/transaction/withdrawl/deposit classes with related driver code

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
-let balance = 500.00;
 
-class Withdrawal {
+// LightCoin (LHL)
 
-  constructor(amount) {
+
+
+// Here's a list of features that our code needs to support:
+///////////////////////////////////
+// Allow multiple accounts to be created
+// Each account can have many transactions
+// Allow withdrawals and deposits into accounts
+// Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+// Allow us to retrieve the current balance of the account at any time
+// Don't allow withdrawals that exceed the remaining balance of the account
+
+
+////////////////////////////// Account class
+class account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
+
+////////////////////////////// Transaction class
+class Transaction  {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
+}
 
+////////////////////////////// Withdrawl class
+class Withdrawal extends Transaction {
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
+}
 
+////////////////////////////// Deposit class
+class Deposit extends Transaction {
+  commit() {
+    this.account.balance += this.amount
+  }
 }
 
 
+const myAccount = new account("Canada's Wonderland")
 
 
+///////////////////////////////////
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+transaction3 = new Deposit(60000, myAccount);
+transaction3.commit();
+console.log('transaction 3:', transaction3)
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+transaction1 = new Withdrawal(50.25, myAccount);
+transaction1.commit();
+console.log('Transaction 1:', transaction1);
 
-console.log('Balance:', balance);
+transaction2 = new Withdrawal(9.99, myAccount);
+transaction2.commit();
+console.log('Transaction 2:', transaction2);
+
+
+
+
